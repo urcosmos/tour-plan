fb6a514dd51ff98286c832390fc5cf89cd8fc2b9
urcosmos <urcosmos@yandex.ru>
2020-12-06 22:35:26 +0300

    create: modal window & adaptive to it

--- js/main.js
+++ js/main.js
@@ -1,37 +1,65 @@
-var hotelSlider = new Swiper('.hotel-slider__container', {
-  // autoplay: {
-  //   delay: 3000
-  // },
-  loop: true,
-  keyboard: {
-    enabled: true
-  },
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-});
+$(document).ready(function () {
 
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // autoplay: {
-  //   delay: 1000
-  // },
-  loop: true,
-  // keyboard: {
-  //   enabled: true
-  // },
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
+  var hotelSlider = new Swiper('.hotel-slider__container', {
+    // autoplay: {
+    //   delay: 3000
+    // },
+    loop: true,
+    keyboard: {
+      enabled: true
+    },
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+  });
 
-// $('.parallax-window').parallax({
-//   imageSrc: '../img/newsletter-bg.jpg'
-// });
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // autoplay: {
+    //   delay: 1000
+    // },
+    loop: true,
+    // keyboard: {
+    //   enabled: true
+    // },
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
 
-var menuButton = document.querySelector('.menu-button');
+  // $('.parallax-window').parallax({
+  //   imageSrc: '../img/newsletter-bg.jpg'
+  // });
 
-menuButton.addEventListener('click', function () {
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
+  var menuButton = $('.menu-button');
+
+  menuButton.on('click', function () {
+    $('.navbar-bottom').toggleClass('navbar-bottom--visible');
+  });
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modal = $('.modal');
+    modal.addClass('modal--visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modal = $('.modal');
+    modal.removeClass('modal--visible');
+  }
+
+  $(document).on('keyup', function (evt) {
+    if (evt.keyCode == 27) {
+      var modal = $('.modal');
+      modal.removeClass('modal--visible');
+    }
+  });
+
+
 });
