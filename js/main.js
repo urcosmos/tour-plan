19e1bae6f06a16c81e118bebf5fa5eae5f50c6b5
urcosmos <urcosmos@yandex.ru>
2020-12-06 15:10:51 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -29,3 +29,9 @@ var reviewsSlider = new Swiper('.reviews-slider', {
 // $('.parallax-window').parallax({
 //   imageSrc: '../img/newsletter-bg.jpg'
 // });
+
+var menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', function () {
+  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
+});
