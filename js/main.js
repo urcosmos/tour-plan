da557f54968cab62c7143a1bd8afca98fa97b548
urcosmos <urcosmos@yandex.ru>
2020-12-09 00:52:35 +0300

    fix: footer telephone & PP & mask for telephone

--- js/main.js
+++ js/main.js
@@ -116,7 +116,7 @@ $(document).ready(function () {
   });
 
   $('.input--phone-masked').each(function () {
-    $(this).mask('+0 (000) 000-00-00');
+    $(this).mask('+7 (000) 000-00-00');
   });
 
 
