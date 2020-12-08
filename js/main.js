345d4c3510ee9fde162bbd3ce1dc27216230e09d
urcosmos <urcosmos@yandex.ru>
2020-12-08 21:36:32 +0300

    create: - connect animation on scroll & set it to activities cards; - connect validator for forms & setup it; - connect mask for telephone inputs; - animation for navbar-bottom links & buttons & modal window.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+  AOS.init();
+
   var hotelSlider = new Swiper('.hotel-slider__container', {
     // autoplay: {
     //   delay: 3000
@@ -46,27 +48,77 @@ $(document).ready(function () {
 
   function openModal() {
     var modal = $('.modal');
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
     var body = $('body');
     modal.addClass('modal--visible');
     body.addClass('body-overflow');
+    setTimeout(function () {
+      modalOverlay.addClass('modal__overlay--visible');
+      modalDialog.addClass('modal__dialog--visible');
+    }, 10);
   }
 
   function closeModal(event) {
     event.preventDefault();
     var modal = $('.modal');
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
     var body = $('body');
-    modal.removeClass('modal--visible');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
     body.removeClass('body-overflow');
+    setTimeout(function () {
+      modal.removeClass('modal--visible');
+    }, 500);
   }
 
   $(document).on('keyup', function (evt) {
     if (evt.keyCode == 27) {
       var modal = $('.modal');
+      var modalOverlay = $('.modal__overlay');
+      var modalDialog = $('.modal__dialog');
       var body = $('body');
-      modal.removeClass('modal--visible');
       body.removeClass('body-overflow');
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+      setTimeout(function () {
+        modal.removeClass('modal--visible');
+      }, 500);
     }
   });
+
+  $('.form').each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Name should be more than 2 characters"
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: "Please specify your phone number"
+      }
+    });
+  });
+
+  $('.newsletter-subscribe').validate({
+    errorClass: "invalid-subscribe",
+    messages: {
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com"
+      }
+    }
+  });
+
+  $('.input--phone-masked').each(function () {
+    $(this).mask('+0 (000) 000-00-00');
+  });
+
 
 
 });
